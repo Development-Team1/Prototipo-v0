aa84927cd91c9c59b9f9b86cfc0ceef8b03c7f63
Katherine <katherine.morea@correounivalle.edu.co>
2026-09-19 00:24:05 -0500

    restringir CORS al frontend de Vercel

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://frontend-miniproyecto1-fe1u-one.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
