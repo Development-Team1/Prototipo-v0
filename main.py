6ad9decb15cedc9ffc122196dadf97a7ed5b8349
Katherine <katherine.morea@correounivalle.edu.co>
2026-09-19 01:52:03 -0500

    feat: usar regex para permitir subdominios de Vercel en CORS

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://frontend-miniproyecto1-fe1u-one.vercel.app",
         "http://localhost:5173",
         "http://localhost:5174",
     ],
+    allow_origin_regex=r"https://frontend-miniproyecto1.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
